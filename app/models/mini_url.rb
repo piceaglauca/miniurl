61cafc8c7cb1874d5db3a721c48295a5175de514
Scott Howard <scott@showard.ca>
2020-10-07 16:18:58 -0700

    added method to MiniUrl model to generate random 5 char string

--- app/models/mini_url.rb
+++ app/models/mini_url.rb
@@ -1,3 +1,21 @@
+URL_LENGTH = 5
+
 class MiniUrl < ApplicationRecord
   has_many :ip
+  before_create :generate_mini_url
+
+  private
+
+  def generate_mini_url
+
+    # generate a random string of URL_LENGTH chars. Continue loop
+    # until a unique one is found.
+    loop do
+      self.short_url = SecureRandom.alphanumeric(URL_LENGTH)
+
+      if ! MiniUrl.exists?(short_url: self.short_url)
+        break
+      end
+    end
+  end
 end
