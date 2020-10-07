36517e82ba7df5bb0cd6cb82fd6e7c89f847add7
Scott Howard <scott@showard.ca>
2020-10-07 16:37:48 -0700

    added links to index page for urls and added column for num_visits. added method to MiniUrl model to facilitate num_visits from Ip model

--- app/models/mini_url.rb
+++ app/models/mini_url.rb
@@ -4,6 +4,10 @@ class MiniUrl < ApplicationRecord
   has_many :ip
   before_create :generate_mini_url
 
+  def num_visits
+    Ip.where(:mini_url_id => self.id).sum(:num_visits)
+  end
+
   private
 
   def generate_mini_url
