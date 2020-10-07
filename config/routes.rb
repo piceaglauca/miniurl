eed8224dc7c53f9ba08c7a9816be3d4e1651254f
Scott Howard <scott@showard.ca>
2020-10-07 16:09:23 -0700

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :mini_urls
+
+  get '/', to: 'mini_urls#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
